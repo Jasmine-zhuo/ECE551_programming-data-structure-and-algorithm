2ce09ec799bf21fc8455776633e11fdfb5446b8e
ww202 <ww202@duke.edu>
2022-09-08 21:20:14 -0400

    assignment 17

--- 017_squares/squares.c
+++ 017_squares/squares.c
@@ -10,7 +10,7 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
   //compute the max of size1 and (x_offset + size2).  Call this w
   int h = max(size1, (y_offset + size2));
   //compute the max of size1 and (y_offset + size2).  Call this h
-  for (int y = 0; y <= h; y++) {
+  for (int y = 0; y < h; y++) {
     //count from 0 to h. Call the number you count with y
     for (int x = 0; x < w; x++) {
       //count from 0 to w. Call the number you count with x
