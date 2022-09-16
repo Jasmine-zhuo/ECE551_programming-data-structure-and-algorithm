cc9af4a3b8ce2def65b68c1fddd840d6ecd5e023
ww202 <ww202@duke.edu>
2022-09-16 14:47:17 -0400

    29

--- 029_power_rec/power.c
+++ 029_power_rec/power.c
@@ -4,13 +4,8 @@
 unsigned power(unsigned x, unsigned y) {
   if (y == 0)
     return 1;
+  if (x == 0 && y == 0)
+    return 1;
   else
     return power(x, y - 1) * x;
-}
-
-int main(void) {
-  unsigned x = 0;
-  unsigned y = 0;
-  power(x, y);
-  return 0;
 }
