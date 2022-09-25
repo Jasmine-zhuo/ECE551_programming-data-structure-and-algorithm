63fd44a25061961b243051e8073f5951e0085f53
ww202 <ww202@duke.edu>
2022-09-25 19:41:07 -0400

    2

--- 037_array_subseq/maxSeq.c
+++ 037_array_subseq/maxSeq.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 size_t maxSeq(int * array, size_t n) {
   size_t count = 0;
-  for (int i = 0, i < n, i++) {
+  for (int i = 0; i < n; i++) {
     if (array[i] < array[i + 1]) {
       count++;
     }
