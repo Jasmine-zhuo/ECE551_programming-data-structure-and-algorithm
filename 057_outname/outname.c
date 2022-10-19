b73e0108d184e6d9d240816b89ac37e922d32df0
ww202 <ww202@duke.edu>
2022-10-19 19:09:52 -0400

    1

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,23 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  int size;
+  size = strlen(inputName);
+  char * p = NULL;
+  p = malloc(sizeof(p) * (size + 7));
+  strncpy(p, inputName, size);
+  *(p + size) = '.';
+  *(p + size + 1) = 'c';
+  *(p + size + 2) = 'o';
+  *(p + size + 3) = 'u';
+  *(p + size + 4) = 'n';
+  *(p + size + 5) = 't';
+  *(p + size + 6) = 's';
+  *(p + size + 7) = '\0';
+  return p;
 }
